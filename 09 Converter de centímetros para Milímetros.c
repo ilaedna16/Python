b08c8fa1f299d36be9f5d5d86ea7ea13e0f7dc85
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-11-14 07:39:44 -0300

    Create 09 Converter de centímetros para Milímetros.c

---
+++ 09 Converter de centímetros para Milímetros.c
@@ -0,0 +1,7 @@
+#Escreva um programa que leia um valor em metros
+#e o exiba em centrímetros e milímetros.
+
+medida=float(input('Uma distância em metros:'))
+cm=medida*100
+mm=medida*1000
+print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(medida,cm,mm))
