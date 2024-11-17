3d4c5e66c46cf07974872099e313e00c4ac88396
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-11-17 08:07:00 -0300

    Create 14 Salário de um funcionário e mostre seu novo salário, com 15% de desconto.c

---
+++ 14 Salário de um funcionário e mostre seu novo salário, com 15% de desconto.c
@@ -0,0 +1,6 @@
+#Faça um algoritmo que leia o salário de um funcionário
+#e mostre seu novo salário, com 15% de aumento. 
+
+salário=float(input('Qual é o slaário do funcionário?'))
+novo=salário+(salário*15/100)
+print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salário,novo))
