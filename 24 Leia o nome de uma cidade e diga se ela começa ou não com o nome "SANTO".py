14c531eeb05e86488dc58c92507c19b2dc557a12
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-11-25 10:25:40 -0300

    Update 24 Leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".py

--- 24 Leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".py
+++ 24 Leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".py
@@ -1,5 +1,5 @@
 #Crie um programa que leia o nome de uma cidade
 #e diga se ela começa ou não com o nome "santo".
 
-cid=str(input('Em qque cidade você nasceu? ')).strip()
+cid=str(input('Em que cidade você nasceu? ')).strip()
 print(cid[:5].upper()== 'SANTO')
