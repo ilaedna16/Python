d5422f73c3efdbe553826f8ac90400512345bffc
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-12-03 09:17:58 -0300

    Create 43 Altura e peso de uma pessoa e calcule o seu IMC e mostre seu status.py

---
+++ 43 Altura e peso de uma pessoa e calcule o seu IMC e mostre seu status.py
@@ -0,0 +1,23 @@
+#Desenvolva uma lógica que leia o peso e altura de uma pessoa,
+#calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:;
+#Abaixo de 18.5: abaixo do peso
+#entre 18.5 e 25: peso ideal
+#25 até 30: sobrepeso
+#30 até 40: obesidade
+#Acima de 40: obesidade mórbida
+
+peso = float(input('qual é o seu peso? (Kg) '))
+altura = float(input('qual é a sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está abaixo do peso normal')
+elif 18.5 <= imc < 25:
+    print('Parabéns, você está na faixa de peso normal')
+elif 25 <= imc < 30:
+    print('Você está em sobrepeso')
+elif 30 <= imc < 40:
+    print('Você está em obesidade!')
+elif imc >= 40:
+    print('Você está em obesidade mórbida, cuidado!')
+
