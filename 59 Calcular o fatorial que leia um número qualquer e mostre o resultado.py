6ff589a07c031c68c72fb29955a6c3d91d3619ce
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2024-12-14 10:03:23 -0300

    Create 59 Calcular o fatorial que leia um número qualquer e mostre o resultado.py

---
+++ 59 Calcular o fatorial que leia um número qualquer e mostre o resultado.py
@@ -0,0 +1,12 @@
+#Calcular o fatorial que leia um número qualquer e mostre o seu fatorial.
+
+n = int(input('digite um número para calcular seu factorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f = f * c
+    c = c - 1
+print('{}'.format (f))
